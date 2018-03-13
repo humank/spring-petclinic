2f62d7ce5b70462300d4473bbdf58d155b7f832c
kimkao <yikaikao@gmail.com>
2018-03-14 01:42:46 +0800

    disable auto gen swagger api document.

--- src/main/java/org/springframework/samples/petclinic/util/ApplicationSwaggerConfig.java
+++ src/main/java/org/springframework/samples/petclinic/util/ApplicationSwaggerConfig.java
@@ -18,27 +18,23 @@ package org.springframework.samples.petclinic.util;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Java config for Springfox swagger documentation plugin
- * 
+ *
  * @author Vitaliy Fedoriv
  *
  */
 
-@Configuration
-@EnableSwagger2
-@ComponentScan(basePackages="org.springframework.samples.petclinic.rest")
+//@Configuration
+//@EnableSwagger2
+//@ComponentScan(basePackages="org.springframework.samples.petclinic.rest")
 public class ApplicationSwaggerConfig {
 
    @Bean
